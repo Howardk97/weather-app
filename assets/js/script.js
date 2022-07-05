77885bb0253b2f9714480d4ce2d76b1ca0753ac1
KimboSlice <kimberlyhoward529@gmail.com>
2022-07-04 22:51:15 -0400

    Added area to display weather and area to display previously searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,16 @@
 var userInputDisplay = $('#city-container');
 var searchBtn = $('#search-btn');
 var APIKey = 'f329568241f9989db1ccb96467bd08dd';
+var weatherDisplay = $('#weather-display');
+
 // var userInputBtn = $('<button>');
 // var userInputText = $('input[name="search-bar"]')
+
+var weatherDisplayTemp = document.createElement('ul');
+var weatherDisplayWind = document.createElement('ul');
+var weatherDisplayHumidity = document.createElement('ul');
+var weatherDisplayIcon = document.createElement('div');
+
 function cityList(event) {
     event.preventDefault();
     
@@ -12,13 +20,48 @@ function cityList(event) {
     console.log(city);
     // create an element button to
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + APIKey;
 
     fetch(queryURL).then(function(res) {
         console.log(res);
         return res.json();
     }).then(function(data) {
         console.log(data);
+
+        var cityBtn = document.createElement('button');
+
+        cityBtn.classList.add("btn");
+
+        cityBtn.classList.add("btn-outline-success");
+
+        cityBtn.setAttribute('style','width: 95%; margin: 2%;');
+
+        cityBtn.textContent = city;
+
+        userInputDisplay.append(cityBtn);
+        // logs temperature into console
+        console.log(data.main.temp);
+
+        weatherDisplay.append(weatherDisplayTemp);
+        weatherDisplayTemp.textContent = 'Temperature: ' + data.main.temp + '°F';
+
+        weatherDisplay.append(weatherDisplayWind);
+        weatherDisplayWind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
+
+        weatherDisplay.append(weatherDisplayHumidity);
+        weatherDisplayHumidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
+
+        var iconCode = data.weather[0].icon;
+
+        console.log(iconCode);
+
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+        $('#weather-icon').attr('src', iconUrl);
+
+
+
+
     }) 
 }
 

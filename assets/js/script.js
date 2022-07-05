59916f568a4104d51cc277b7b84be3d18c75d67d
KimboSlice <kimberlyhoward529@gmail.com>
2022-07-05 17:59:32 -0400

    I added a header and styling to the weather display section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,91 @@
 // Global variables
-var userInputDisplay = $('#city-container');
-var searchBtn = $('#search-btn');
+var userInputDisplay = document.getElementById('city-container');
+var searchBtn = document.getElementById('search-btn');
 var APIKey = 'f329568241f9989db1ccb96467bd08dd';
-var weatherDisplay = $('#weather-display');
+var weatherDisplay = document.getElementById('weather-display');
 
-// var userInputBtn = $('<button>');
-// var userInputText = $('input[name="search-bar"]')
+weatherDisplay.style.display = "none";
 
-var weatherDisplayTemp = document.createElement('ul');
-var weatherDisplayWind = document.createElement('ul');
-var weatherDisplayHumidity = document.createElement('ul');
-var weatherDisplayIcon = document.createElement('div');
+// Variables for weather info
+var weatherDisplayTemp = document.createElement('p');
+var weatherDisplayWind = document.createElement('p');
+var weatherDisplayHumidity = document.createElement('p');
 
-function cityList(event) {
+// Variables for city header
+var cityTitle = document.createElement('h1');
+var cityDate = document.createElement('h1');
+var todayDate = moment().format("MMM Do, YYYY");
+var cityIcon = document.createElement('img');
+
+// Function generated when search is clicked
+function searchResponse(event) {
     event.preventDefault();
+    weatherDisplay.style.display = "block";
     
-    var city = $('input[name="search-bar"]').val();
+    var city = document.getElementById('search-input').value;
 
-    console.log(city);
+    localStorage.setItem("city", city);
+
+    // console.log(cityArray);
+
+    // console.log(city);
     // create an element button to
 
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + APIKey;
 
-    fetch(queryURL).then(function(res) {
-        console.log(res);
+    fetch(queryURL)
+    .then(function(res) {
         return res.json();
-    }).then(function(data) {
-        console.log(data);
-
+    })
+    .then(function(data) {
+        // buttons for previously searched cities
         var cityBtn = document.createElement('button');
+        console.log(data.weather[0].icon);
 
+        // Style previously searched city buttons
         cityBtn.classList.add("btn");
-
         cityBtn.classList.add("btn-outline-success");
+        cityBtn.classList.add("col-12");
+        cityBtn.classList.add("col-md-8");
+        cityBtn.setAttribute('style','margin: 2%; border-color: grey; background-color: grey; color: white');
 
-        cityBtn.setAttribute('style','width: 95%; margin: 2%;');
-
+        // Display text onto previously searched city buttons
         cityBtn.textContent = city;
 
-        userInputDisplay.append(cityBtn);
-        // logs temperature into console
-        console.log(data.main.temp);
+        // Append previously searched city buttons to page
+        userInputDisplay.appendChild(cityBtn); 
+        
+        var cityHeader = document.createElement('div');
 
-        weatherDisplay.append(weatherDisplayTemp);
+        // attach weather icon to city header
+        cityIcon.setAttribute('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png');
+
+        // add text to elements
+        cityTitle.textContent = city;
+        cityDate.textContent = '(' + todayDate + ')';
+        
+
+        // attach to webpage
+        cityHeader.appendChild(cityTitle);
+        cityHeader.appendChild(cityDate);
+        weatherDisplay.appendChild(cityHeader);
+        cityHeader.appendChild(cityIcon);
+
+        // Add class to city header
+        cityHeader.classList.add("row");
+
+        // Append weather data to page
+        weatherDisplay.appendChild(weatherDisplayTemp);
         weatherDisplayTemp.textContent = 'Temperature: ' + data.main.temp + '°F';
-
-        weatherDisplay.append(weatherDisplayWind);
+        weatherDisplay.appendChild(weatherDisplayWind);
         weatherDisplayWind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
-
-        weatherDisplay.append(weatherDisplayHumidity);
+        weatherDisplay.appendChild(weatherDisplayHumidity);
         weatherDisplayHumidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
 
-        var iconCode = data.weather[0].icon;
+        weatherDisplay.setAttribute('style', 'background: lightblue;')
 
-        console.log(iconCode);
-
-        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-        $('#weather-icon').attr('src', iconUrl);
-
-
-
-
+        
     }) 
 }
 
-searchBtn.on('click', cityList);
-// userInputDisplay.append(userInputBtn);
-
-// userInputBtn.append(userInputText);
-
-// console.log(userInputDisplay);
-
-
-
-
-
-// Steps
-// 1. Get an API that retrieves lat and long if given a city name 
-// 2. Use location API to get latitude and longitude of user input city
-// 3. Make a conditional that if latitude and longitude are numbers, then enter them in weather API, else prompt user to enter another city name
+searchBtn.addEventListener('click', searchResponse);
